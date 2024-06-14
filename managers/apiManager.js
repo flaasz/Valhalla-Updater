df5a63b51eb1124035766b3d1c87317446920cf9
Bartłomiej Profic <bp8258@gmail.com>
2024-06-14 02:00:29 +0200

    fixed api to send alerts correctly

--- managers/apiManager.js
+++ managers/apiManager.js
@@ -4,7 +4,7 @@
  * File Created: Friday, 14th June 2024 12:29:18 am
  * Author: flaasz
  * -----
- * Last Modified: Friday, 14th June 2024 1:32:47 am
+ * Last Modified: Friday, 14th June 2024 1:59:35 am
  * Modified By: flaasz
  * -----
  * Copyright 2024 flaasz
@@ -13,7 +13,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const webhook = require('../discord/webhook');
 const webApiPort = require("../config/config.json").webApi.port;
+require('dotenv').config();
+const kofiToken = process.env.KOFI_TOKEN;
+const config = require("../config/config.json");
+const pterodactyl = require('../modules/pterodactyl');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -22,11 +27,10 @@ app.use(bodyParser.json());
 module.exports = {
     startServer: async function () {
 
-        let output = {
-            name: "",
+        const output = {
             content: "",
-            avatar: "",
-            bonus: "",
+            username: "Ko-fi",
+            avatarURL: "https://uploads-ssl.webflow.com/5c14e387dab576fe667689cf/61e1116779fc0a9bd5bdbcc7_Frame%206.png",
         };
 
         app.post('/post', async function (req, res) {
@@ -41,18 +45,17 @@ module.exports = {
                 const obj = JSON.parse(data);
                 console.log(obj);
                 if (obj.verification_token != kofiToken) return;
-                output.name = "Ko-fi";
                 if (obj.message) {
                     output.content = `**${obj.from_name}** just donated **€${obj.amount}**: *${obj.message}*`;
-                    //sendToMc.alert(`[Ko-fi] ${obj.from_name} just donated €${obj.amount}: ${obj.message}`);
+                    pterodactyl.sendCommand(config.pterodactyl.velocityID, `say [Ko-fi] ${obj.from_name} just donated €${obj.amount}: ${obj.message}`);
+                    console.log(`alert [Ko-fi] ${obj.from_name} just donated €${obj.amount}: ${obj.message}`);
                 } else {
                     output.content = `**${obj.from_name}** just donated **€${obj.amount}**!`;
-                    //sendToMc.alert(`[Ko-fi] ${obj.from_name} just donated €${obj.amount}!`);
+                    pterodactyl.sendCommand(config.pterodactyl.velocityID, `say [Ko-fi] ${obj.from_name} just donated €${obj.amount}!`);
+                    console.log(`alert [Ko-fi] ${obj.from_name} just donated €${obj.amount}!`);
                 }
-                output.avatar = "https://uploads-ssl.webflow.com/5c14e387dab576fe667689cf/61e1116779fc0a9bd5bdbcc7_Frame%206.png";
-                //sendMessage(output);
+                webhook.sendWebhook(config.discord.chatChannelId, output);
 
-                //encoder.encodeAnnouncement(output.content); 
             } catch (err) {
                 console.error(err);
                 return res.json({
